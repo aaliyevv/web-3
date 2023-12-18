73fb0eac86f275200268612c041b86fe4d801c52
Ali Aliyev <alialiyev@Ali-MacBook-Pro-2.local>
2023-12-18 23:45:09 +0400

    Added the required forms to contact me page by making a lot of improvements to ContactMePage.js

--- src/ContactMePage.js
+++ src/ContactMePage.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ContactMePage() {
+  const [subject, setSubject] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ subject, email, message });
+  };
+
   return (
     <div>
       <h1>Contact Me Page</h1>
       <p>If you have any questions, feel free to reach out!</p>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="subject">Subject:</label>
+          <input
+            type="text"
+            id="subject"
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea
+            id="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </div>
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 }
