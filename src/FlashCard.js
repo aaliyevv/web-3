acd0166a63dbbabe8cb5a88984d59ebdf2be9d30
Ali Aliyev <alialiyev@Ali-MacBook-Pro-2.local>
2023-12-22 21:57:16 +0400

    Began to work on the flashcards page and added a sample flashcard with to sides, last modification date, and a status which can be changed.

---
+++ src/FlashCard.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+function FlashCard({ card, onStatusChange }) {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const flipCard = () => setIsFlipped(!isFlipped);
+
+  return (
+    <div onClick={flipCard}>
+      <div>{isFlipped ? card.answer : card.question}</div>
+      <div>Last Modified: {card.lastModified}</div>
+      <div>Status: {card.status}</div>
+      <button onClick={() => onStatusChange(card.id, "Learned")}>Learned</button>
+      <button onClick={() => onStatusChange(card.id, "Want to Learn")}>Want to Learn</button>
+      <button onClick={() => onStatusChange(card.id, "Noted")}>Noted</button>
+    </div>
+  );
+}
+
+export default FlashCard;
--- src/FlashCardsPage.js
+++ src/FlashCardsPage.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import FlashCard from './FlashCard'; 
 
 function FlashCardsPage() {
+  const [cards, setCards] = useState([
+    {
+      id: 1,
+      question: "What is React?",
+      answer: "A JavaScript library for building user interfaces",
+      lastModified: new Date().toISOString(),
+      status: "Want to Learn"
+    },
+  ]);
+
+  const handleStatusChange = (cardId, newStatus) => {
+    setCards(cards.map(card =>
+      card.id === cardId ? { ...card, status: newStatus, lastModified: new Date().toISOString() } : card
+    ));
+  };
+
   return (
     <div>
       <h1>Flash Cards Page</h1>
       <p>Here you can practice with flash cards.</p>
+      <div>
+        {cards.map(card => (
+          <FlashCard
+            key={card.id}
+            card={card}
+            onStatusChange={handleStatusChange}
+          />
+        ))}
+      </div>
     </div>
   );
 }
